e98a03b220bfcbec2de51cae7da53544f94ab087
williamdelcid <dm18019@ues.edu.sv>
2024-07-06 08:04:19 -0600

    Added ng boostraps and boostraps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SkeletonComponent } from './layouts/skeleton/skeleton.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:SkeletonComponent,
+    children:[
+      {
+        path:'',
+        loadChildren:()=>import('@home/home.module').then(m=>m.HomeModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
